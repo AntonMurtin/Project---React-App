5f7c8a40bc109f42a4db60b79b8d3dcfd4639e89
Anton <140314098+AntonMurtin@users.noreply.github.com>
2023-11-28 10:55:21 +0200

    add catalog page and hame page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Home } from './components/Home/Home';
 import { AuthProvider } from './context/AuthContext';
 import { Register } from './components/Register/Register';
 import { Login } from './components/Login/login';
+import { Catalog } from './components/Catalog/Catalog';
 
 
 function App() {
@@ -15,7 +16,8 @@ function App() {
         <Header />
         <main >
           <Routes>
-            <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
+            <Route path='/catalog' element={<Catalog />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register/>} />
 
---
+++ client/src/components/Catalog/Catalog.js
@@ -0,0 +1,98 @@
+import { Link } from 'react-router-dom'
+
+export const Catalog = () => {
+
+    return (
+        <section className="catalog">
+
+
+            <div className="cards">
+
+                <article className="card card--1">
+
+                    <div className="card__img"></div>
+                    <Link href="catalog/" className="card_link">
+                        <div className="card__img--hover"></div>
+                    </Link>
+                    <div className="card__info">
+
+                        <h3 className="card__title">Crisp Spanish tortilla Matzo brei</h3>
+                        <span className="card__by">by <a href="#" className="card__author" title="author">John Doe</a></span>
+                    </div>
+                </article>
+
+
+                <article className="card card--2">
+
+                    <div className="card__img"></div>
+                    <a href="#" className="card_link">
+                        <div className="card__img--hover"></div>
+                    </a>
+                    <div className="card__info">
+                       
+                        <h3 className="card__title">Discover the sea</h3>
+                        <span className="card__by">by <a href="#" className="card__author" title="author">John Doe</a></span>
+                    </div>
+                </article>
+
+
+                <article className="card card--3">
+
+                    <div className="card__img"></div>
+                    <a href="#" className="card_link">
+                        <div className="card__img--hover"></div>
+                    </a>
+                    <div className="card__info">
+                      
+                        <h3 className="card__title">Crisp Spanish tortilla Matzo brei</h3>
+                        <span className="card__by">by <a href="#" className="card__author" title="author">Celeste Mills</a></span>
+                    </div>
+                </article>
+
+            </div>
+            <div className="cards">
+
+                <article className="card card--4">
+
+                    <div className="card__img"></div>
+                    <a href="#" className="card_link">
+                        <div className="card__img--hover"></div>
+                    </a>
+                    <div className="card__info">
+                        
+                        <h3 className="card__title">Crisp Spanish tortilla Matzo brei</h3>
+                        <span className="card__by">by <a href="#" className="card__author" title="author">Celeste Mills</a></span>
+                    </div>
+                </article>
+
+
+                <article className="card card--5">
+
+                    <div className="card__img"></div>
+                    <a href="#" className="card_link">
+                        <div className="card__img--hover"></div>
+                    </a>
+                    <div className="card__info">
+                       
+                        <h3 className="card__title">Discover the sea</h3>
+                        <span className="card__by">by <a href="#" className="card__author" title="author">John Doe</a></span>
+                    </div>
+                </article>
+                <article className="card card--6">
+
+                    <div className="card__img"></div>
+                    <a href="#" className="card_link">
+                        <div className="card__img--hover"></div>
+                    </a>
+                    <div className="card__info">
+                      
+                        <h3 className="card__title">Crisp Spanish tortilla Matzo brei</h3>
+                        <span className="card__by">by <a href="#" className="card__author" title="author">Celeste Mills</a></span>
+                    </div>
+                </article>
+
+            </div>
+
+        </section>
+    )
+}
